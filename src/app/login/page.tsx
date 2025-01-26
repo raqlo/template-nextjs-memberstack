81decc4075eba3608af925fb40966f64ee8f0c0b
Raquel Lopez <raquelcle@gmail.com>
2025-01-26 00:17:09 -0400

    wip: Structure initial user navigation

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+import Button from "@/components/button";
+
+const LoginPage: NextPage = () => {
+    return <div>
+            Login
+        <Button></Button>
+        </div>
+
+}
+
+export default LoginPage;
---
+++ src/components/button.tsx
@@ -0,0 +1,3 @@
+const Button = () => (<button>Hello World</button>)
+
+export default Button;
